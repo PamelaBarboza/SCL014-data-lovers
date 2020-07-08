a47ca43b6dbeb4d081ce974773e9a723ecb96c38
Pamela <66397527+PamelaBarboza@users.noreply.github.com>
2020-07-08 17:11:18 -0400

    Filtro de Hechizo console.log

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import { filterWands } from './data.js';
+import { filterWands, filterSpells } from './data.js';
 import POTTER from './data/potter/potter.js';
 
 console.log(POTTER);
@@ -45,29 +45,36 @@ wandsMenu.addEventListener('click', () => {
   console.log(arrWands);
 });
 
-
+// Función que muestra los Hechizos
 const spellsMenu = document.getElementById('spellsMenu');
 spellsMenu.addEventListener('click', () => {
+  const arrSpells = filterSpells(POTTER);
+  document.getElementById('insertPotterSpells').innerHTML = showMainPotterCharacters(arrSpells);
+  console.log(arrSpells);
 });
+
+// Función que muestra la casa G
 const subOption1 = document.getElementById('subOption1');
 subOption1.addEventListener('click', () => {
 });
-
+// Función que muestra la casa S
 const subOption2 = document.getElementById('subOption2');
 subOption2.addEventListener('click', () => {
 });
+
+// Función que muestra la casa H
 const subOption3 = document.getElementById('subOption3');
 subOption3.addEventListener('click', () => {
 });
-
+// Función que muestra la casa R
 const subOption4 = document.getElementById('subOption4');
 subOption4.addEventListener('click', () => {
 });
-
+// Función que muestra Profesores
 const subOption5 = document.getElementById('subOption5');
 subOption5.addEventListener('click', () => {
 });
-
+// Función que muestra la casa Estudiantes
 const subOption6 = document.getElementById('subOption6');
 subOption6.addEventListener('click', () => {
 });
